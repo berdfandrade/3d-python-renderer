75603a8a1d977fcefedc96188f8c27b7335d450b
Bernardo Andrade <berdfandrade@gmail.com>
2024-01-26 10:35:40 -0300

    --

--- viewer.py
+++ viewer.py
@@ -1,8 +1,6 @@
 class Viewer(object):
     def __init__(self):
-        
-        """Initialze the viewer"""
-        
+        """ Initialize the viewer. """
         self.init_interface()
         self.init_opengl()
         self.init_scene()
@@ -10,12 +8,7 @@ class Viewer(object):
         init_primitives()
 
     def init_interface(self):
-        
-        """
-        initialize the window and 
-        register the render function
-        """
-        
+        """ initialize the window and register the render function """
         glutInit()
         glutInitWindowSize(640, 480)
         glutCreateWindow("3D Modeller")
@@ -23,11 +16,7 @@ class Viewer(object):
         glutDisplayFunc(self.render)
 
     def init_opengl(self):
-        
-        """
-        initialize the opengl 
-        settings to render the scene
-        """
+        """ initialize the opengl settings to render the scene """
         self.inverseModelView = numpy.identity(4)
         self.modelView = numpy.identity(4)
 
@@ -43,40 +32,39 @@ class Viewer(object):
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
         glEnable(GL_COLOR_MATERIAL)
         glClearColor(0.4, 0.4, 0.4, 0.0)
-    
+
     def init_scene(self):
         """ initialize the scene object and initial scene """
         self.scene = Scene()
         self.create_sample_scene()
-    
-    def create_sameple_scene(self):
+
+    def create_sample_scene(self):
         cube_node = Cube()
-        cube.node.translate(2,0,2)
+        cube_node.translate(2, 0, 2)
         cube_node.color_index = 2
         self.scene.add_node(cube_node)
-        
+
         sphere_node = Sphere()
-        sphere_node.translate(-2,0,2)
-        shpere_node.color_index = 3
+        sphere_node.translate(-2, 0, 2)
+        sphere_node.color_index = 3
         self.scene.add_node(sphere_node)
-        
+
         hierarchical_node = SnowFigure()
-        hierarchical_node.translate(-2,0,2)
+        hierarchical_node.translate(-2, 0, -2)
         self.scene.add_node(hierarchical_node)
-    
-    def init_viewer(self):
-        
-        """
-        initialize the projection matrix
-        """
 
-        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGET(GET_WINDOW_HEIGTH)
-        aspect_ratio = float(xSize) / float(ySize)
+    def init_interaction(self):
+        """ init user interaction and callbacks """
+        self.interaction = Interaction()
+        self.interaction.register_callback('pick', self.pick)
+        self.interaction.register_callback('move', self.move)
+        self.interaction.register_callback('place', self.place)
+        self.interaction.register_callback('rotate_color', self.rotate_color)
+        self.interaction.register_callback('scale', self.scale)
 
-        # Load the projection matrix. Always the same
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
+    def main_loop(self):
+        glutMainLoop()
 
-        glViewport(0, 0, xSize, ySize)
-        gluPerspective(70, aspect_ratio, 0.1, 1000.0)
-        glTranslated(0, 0, -15)
+if __name__ == "__main__":
+    viewer = Viewer()
+    viewer.main_loop()

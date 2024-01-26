1abb1e5dc484d27b5f99494e0dff67a7168b3759
Bernardo Andrade <berdfandrade@gmail.com>
2024-01-26 10:34:14 -0300

    --

--- viewer.py
+++ viewer.py
@@ -34,7 +34,36 @@ class Viewer(object):
         glEnable(GL_CULL_FACE)
         glCullFace(GL_BACK)
         glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LESS)
 
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 0, 1, 0))
+        glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, -1))
+
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable(GL_COLOR_MATERIAL)
+        glClearColor(0.4, 0.4, 0.4, 0.0)
+    
+    def init_scene(self):
+        """ initialize the scene object and initial scene """
+        self.scene = Scene()
+        self.create_sample_scene()
+    
+    def create_sameple_scene(self):
+        cube_node = Cube()
+        cube.node.translate(2,0,2)
+        cube_node.color_index = 2
+        self.scene.add_node(cube_node)
+        
+        sphere_node = Sphere()
+        sphere_node.translate(-2,0,2)
+        shpere_node.color_index = 3
+        self.scene.add_node(sphere_node)
+        
+        hierarchical_node = SnowFigure()
+        hierarchical_node.translate(-2,0,2)
+        self.scene.add_node(hierarchical_node)
+    
     def init_viewer(self):
         
         """
